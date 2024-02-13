05e34be3734d3eabacf4303c9252758e6c4b0529
schaeferle <elias-schaefer@live.de>
2024-02-13 18:26:10 +0100

    Finale Aktualisierung

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-//coole änderung juhu
+//script.js für index.html
+// Erstellt von ELias Schäfer (268512) für den Kurs EIA2
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Das Skript wird ausgeführt!");
+document.addEventListener('DOMContentLoaded', function() {      // Stelle sicher, dass alle Elemente im DOM geladen werden
+    console.log("Das Skript wird ausgeführt!");                 // Zeig mir in der Konsole ob das Skript überhaupt ausgeführt wird
 
     var currentlyVisibleText = null;
 
+    // Funktion zum Ein- und AUsblenden der Infotexte beim Anklicken
     function toggleTextVisibility(textEl, cursorEl) {
         if (currentlyVisibleText !== null && currentlyVisibleText !== textEl) {
             currentlyVisibleText.setAttribute('visible', false);
@@ -21,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 cursorEl.setAttribute('visible', true);
                 currentlyVisibleText = null;
             } else {
-                // Wenn der Text nicht sichtbar ist, mache ihn sichtbar
+            // Wenn der Text nicht sichtbar ist, mache ihn sichtbar
                 textEl.setAttribute('visible', true);
                 cursorEl.setAttribute('visible', false);
                 currentlyVisibleText = textEl;
@@ -29,35 +31,35 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // Korb Text
+    // Der Korb-Text erscheint wenn der Korb angeklickt wird
     document.getElementById('korb').addEventListener('click', function() {
         var textEl = document.getElementById('textKorb');
         var cursorEl = document.getElementById('cursor');
         toggleTextVisibility(textEl, cursorEl);
     });
 
-    // Hülle Text
+    // Der Hülle-Text erscheint wenn die Hülle angeklickt wird
     document.getElementById('huelle').addEventListener('click', function() {
         var textEl = document.getElementById('textHuelle');
         var cursorEl = document.getElementById('cursor');
         toggleTextVisibility(textEl, cursorEl);
     });
 
-    // Brenner Text
+    // Der Brenner-Text erscheint wenn der Brenner angeklickt wird
     document.getElementById('brenner').addEventListener('click', function() {
         var textEl = document.getElementById('textBrenner');
         var cursorEl = document.getElementById('cursor');
         toggleTextVisibility(textEl, cursorEl);
     });
 
-    // Seile Text
+    // Der Lastleinen-Text erscheint wenn die Lastleinen angeklickt werden
     document.getElementById('seile').addEventListener('click', function() {
         var textEl = document.getElementById('textSeile');
         var cursorEl = document.getElementById('cursor');
         toggleTextVisibility(textEl, cursorEl);
     });
 
-    // Gerüst Text
+    // Der Gerüst-Text erscheint wenn das Gerüst angeklickt wird
     document.getElementById('geruest').addEventListener('click', function() {
         var textEl = document.getElementById('textGeruest');
         var cursorEl = document.getElementById('cursor');
